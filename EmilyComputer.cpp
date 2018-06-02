095fa5009b67a4a328773df54c601c9e8dd85692
Emily Crook <ehcrook@gmail.com>
2018-06-02 18:06:10 -0400

    a mediocre computer player

--- EmilyComputer.cpp
+++ EmilyComputer.cpp
@@ -1,4 +1,58 @@
 /* Emily's computer player */
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <cstdlib>
+
+#include "EmilyComputer.h"
+#include "Card.h"
+#include "Hand.h"
+
 using namespace std;
+
+
+string EmilyComputer::play(Hand hand, Hand shared) {
+	set<string> playable;
+
+    string letters = hand.getLetters() + shared.getLetters();
+    
+    for(int length=10; length != 2; length--) {
+    	int n = 1000;
+    	if(length == 3) n = 100;
+        tryWords(playable, letters, length, n);
+    }
+    
+    if(playable.size() == 0) return "D";
+
+    int num = rand()%playable.size();
+    set<string>::iterator itr = playable.begin();
+
+    for(int i = 0; i < num; i++)
+    	itr++;
+
+    return *itr;
+}
+
+void EmilyComputer::tryWords(set<string>& playable, string letters, int length, int attempts) {
+    
+    int i = 0;
+    string word;
+    while (i<attempts) {
+        random_shuffle(letters.begin(), letters.end());
+        word = letters.substr(0,length);
+      	if(dictionary.check(word))
+      		playable.insert(word);
+        i++;
+    }
+}
+
+string EmilyComputer::discard(Hand hand) {
+    hand.shuffle();
+    return hand.getLetters().substr(0,1);
+}
+
+Card EmilyComputer::buyPatent(vector<Card> patents) {
+    return patents[0];
+}
--- EmilyComputer.h
+++ EmilyComputer.h
@@ -1,9 +1,25 @@
 /* Emily's computer player */
 
-#include <iostream>
+#ifndef _EMILYCOMPUTER_H_
+#define _EMILYCOMPUTER_H_
+
+#include "Card.h"
+#include "Hand.h"
+#include "Strategy.h"
+#include "Dictionary.h"
 using namespace std;
 
-class EmilyComputer
+class EmilyComputer: public Strategy
 {
-	
+public:
+    EmilyComputer(Dictionary &d) : Strategy(d) {}
+    string play(Hand hand, Hand shared);
+    string discard(Hand hand);
+    Card buyPatent(vector<Card> patents);    
+private:
+    void tryWords(set<string>& playable, string letters, int length, int attempts);
+
 };
+
+
+#endif
